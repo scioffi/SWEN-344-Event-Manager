eafda4bd4fb0cb667fc637f340d0f6a17c900caa
Marianna Sternefeld <marianna@sternefeld.com>
2018-02-17 19:52:38 -0500

    Added basic bootstrap

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
-const express = require('express');
-const app = express();
+var express = require("express");
+var app = express();
+var router = express.Router();
+var path = __dirname + '/views/';
 
-app.get('/', (req, res) => res.send('<a href="http://partyhorse.party">PartyHorse.Party</a> <-- Highly recommended'));
+router.use(function (req,res,next) {
+  console.log("/" + req.method);
+  next();
+});
 
-app.listen(80);
+router.get("/",function(req,res){
+  res.sendFile(path + "index.html");
+});
+
+
+app.use("/",router);
+
+app.use("*",function(req,res){
+  res.sendFile(path + "404.html");
+});
+
+app.listen(3000,function(){
+  console.log("Live at Port 3000");
+});
