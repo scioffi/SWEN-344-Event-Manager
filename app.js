1be655e3bd962a3e1aa9cb464ef28d2508d56fec
Stephen Cioffi <stephen@stephencioffi.com>
2018-03-01 15:20:44 -0500

    Mhm. I make a nice form :D

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();  //use express js module
+var moment = require('moment');
 
 // Static files to not be parsed by Node
 app.use('/files', express.static(__dirname + '/files'));
--- frontend-routing.js
+++ frontend-routing.js
@@ -2,7 +2,12 @@ module.exports = function(app) {
 	app.get('/', function(req,res){ 
 		res.render('index');  //respond with homepage
 	});
+
 	app.get('/event', function(req,res){ 
 		res.render('event');  //respond with event page
 	});
-}
+
+	app.get("/NewEvent", function(req, res){
+		res.render("EventForm");
+	});
+};
