48662e1f9df2639e3010330849065907c6eccfce
Stephen Cioffi <stephen@stephencioffi.com>
2018-03-01 23:20:53 -0500

    We have data!

--- frontend-routing.js
+++ frontend-routing.js
@@ -1,10 +1,28 @@
+function getData(url){
+    fetch(url, {
+    	method: "get",
+		credentials: "include"
+	})
+	.then(function(response) {
+		return response.json();
+	})
+	.error(function(e) {
+		console.error(e);
+		return {error: e}
+	});
+}
+
 module.exports = function(app) {
-	app.get('/', function(req,res){ 
+	app.get('/', function(req, res){
 		res.render('index');  //respond with homepage
 	});
 
-	app.get('/event', function(req,res){ 
-		res.render('event');  //respond with event page
+	app.get('/event', function(req, res){
+		var data = getData("/api/getEvent", {});
+
+		res.render('event', {
+			data: data
+		});  //respond with event page
 	});
 
 	app.get("/NewEvent", function(req, res){
