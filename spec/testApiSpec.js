eef095098686e8447ce34969422b450224771271
Moses Lagoon <mxl2261@g.rit.edu>
2018-03-03 23:38:04 -0500

    test updated for get user to incorporate parameters

--- spec/testApiSpec.js
+++ spec/testApiSpec.js
@@ -1,7 +1,7 @@
 var request = require("request");
 
 var base_url = "http://localhost:8080"
-var get_user_url = "http://localhost:8080/getUser"
+var get_user_url = base_url + "/getUser"
 
 // var TestApiHelper = require('./helper/TestApiHelper.js');
 // var testMyApi = new TestApiHelper();
@@ -15,32 +15,30 @@ describe("An example suite", function() {
 
 
 // Test Local Server 
-describe("Test Event Manager on Local Server", function(){
+describe("Test Event Manager's Local Server", function(){
 
   // Test Base URL  http://localhost:8080
   describe("GET Base URL /", function(){
     it("returns  status code 200", function(){
       request.get(base_url, function(error, response, body){
-        console.log(response.statusCode);
         expect(response.statusCode).toBe(200);
-        // done();
       });
     });
   });
 });
 
-  // Test /getUser endpoint 
-  describe("GET /getUser", function() {
-    it("returns status code 200", function(done) {                  
-      request.get(get_user_url, function(error, response, body) {
-        console.log(body);
+  // Test /getUser endpoint where id = 1
+  describe("GET /getUser id = 1", function() {
+    var get_user_1 = get_user_url + "?userId=1"   
+    it("returns status code 200", function(done) {              
+      request.get(get_user_1, function(error, response, body) {
         expect(response.statusCode).toBe(200);
         done();
       });
     });
 
     it("returns user JSON", function(done) {
-      request.get(get_user_url, function(error, response, body) {
+      request.get(get_user_1, function(error, response, body) {
         let json = JSON.parse(body)
         expect(body).toBe('{"userId":1,"username":"jdoe1234","name":"John Doe","email":"jdoe1234@rit.edu","permission":"user"}');
         expect(json["userId"]).toBe(1);
