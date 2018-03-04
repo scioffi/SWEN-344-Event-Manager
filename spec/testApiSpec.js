63d10737848587e4580acfed20f1dbfc001cb528
Moses Lagoon <mxl2261@g.rit.edu>
2018-03-03 23:50:33 -0500

    Addressed comments and more tests added plus clean ups

--- spec/testApiSpec.js
+++ spec/testApiSpec.js
@@ -2,6 +2,7 @@ var request = require("request");
 
 var base_url = "http://localhost:8080"
 var get_user_url = base_url + "/getUser"
+var get_event_url = base_url + "/getEvent"
 
 // var TestApiHelper = require('./helper/TestApiHelper.js');
 // var testMyApi = new TestApiHelper();
@@ -9,6 +10,7 @@ var get_user_url = base_url + "/getUser"
 // Jasmine test example 
 describe("An example suite", function() {
   it("contains spec with an expectation", function() {
+
     expect(true).toBe(true);
   });
 });
@@ -17,8 +19,11 @@ describe("An example suite", function() {
 // Test Local Server 
 describe("Test Event Manager's Local Server", function(){
 
-  // Test Base URL  http://localhost:8080
+  // Test Base URL  
   describe("GET Base URL /", function(){
+    console.log("WELCOME TO THE EVENTS MANAGER TESTS!!!!")
+    console.log(" ")
+    console.log("Testing base url:  " + base_url)
     it("returns  status code 200", function(){
       request.get(base_url, function(error, response, body){
         expect(response.statusCode).toBe(200);
@@ -30,6 +35,7 @@ describe("Test Event Manager's Local Server", function(){
   // Test /getUser endpoint where id = 1
   describe("GET /getUser id = 1", function() {
     var get_user_1 = get_user_url + "?userId=1"   
+    console.log("Testing : " + get_user_1)
     it("returns status code 200", function(done) {              
       request.get(get_user_1, function(error, response, body) {
         expect(response.statusCode).toBe(200);
@@ -49,6 +55,33 @@ describe("Test Event Manager's Local Server", function(){
       });
     });
   });
+
+  // Test /getEvent endpoint where id = 1
+  describe("GET /getEvent id = 1", function() {
+    var get_event_1 = get_event_url + "?eventId=1"   
+    console.log("Testing : " + get_event_1)
+    it("returns status code 200", function(done) {              
+      request.get(get_event_1, function(error, response, body) {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+    });
+
+    // test the first event 
+    it("returns user JSON", function(done) {
+      request.get(get_event_1, function(error, response, body) {
+        let json = JSON.parse(body)
+        expect(json["eventId"]).toBe(1);
+        expect(json["title"]).toBe("RIT Spring Fest");
+        expect(json["description"]).toBe("RIT annual event");
+        expect(json["location"]).toBe("RIT campus");
+        expect(json["status"]).toBe("open");
+        done();
+      });
+    });
+  });
+
+
 
 
 
